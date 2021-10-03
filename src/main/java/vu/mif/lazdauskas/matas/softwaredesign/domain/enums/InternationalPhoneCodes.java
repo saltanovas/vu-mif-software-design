d064a969d79691e9931258d9b04df00dab3aa228
saltanovas <zumbass@gmail.com>
2021-10-03 13:29:08 +0300

    Remove national prefix. Add separate validation rule for Lithuania.

--- src/main/java/vu/mif/lazdauskas/matas/softwaredesign/domain/enums/InternationalPhoneCodes.java
+++ src/main/java/vu/mif/lazdauskas/matas/softwaredesign/domain/enums/InternationalPhoneCodes.java
@@ -3,39 +3,46 @@ package vu.mif.lazdauskas.matas.softwaredesign.domain.enums;
 import java.util.Arrays;
 
 public enum InternationalPhoneCodes {
-    LITHUANIA("+370", 12, "8", 9),
-    UK("+44", 13, "7", 12);
+    LITHUANIA("+370", 12) {
+        @Override
+        public boolean isValid(String phoneNumber) {
+            return super.isValid(phoneNumber) || super.isValid(convertToInternationalNumber(phoneNumber));
+        }
+
+        private String convertToInternationalNumber(String phoneNumber) {
+            return phoneNumber != null && phoneNumber.startsWith("8")
+                    ? getInternationalPrefix() + phoneNumber.substring(1)
+                    : null;
+        }
+    },
+    UK("+44", 13);
 
     private final String internationalPrefix;
     private final int internationalLength;
-    private final String nationalPrefix;
-    private final int nationalLength;
 
-    InternationalPhoneCodes(String internationalPrefix, int internationalLength, String nationalPrefix, int nationalLength) {
+    InternationalPhoneCodes(String internationalPrefix, int internationalLength) {
         this.internationalPrefix = internationalPrefix;
         this.internationalLength = internationalLength;
-        this.nationalPrefix = nationalPrefix;
-        this.nationalLength = nationalLength;
     }
 
-    public static InternationalPhoneCodes getPhoneType(String internationalPrefix) {
-        return Arrays.stream(values()).filter(x -> x.internationalPrefix.equals(internationalPrefix)).findFirst().orElse(null);
+    public static InternationalPhoneCodes findFirstOrNull(String internationalPrefix) {
+        return Arrays.stream(values())
+                .filter(x -> x.internationalPrefix.equals(internationalPrefix))
+                .findFirst()
+                .orElse(null);
     }
 
     public boolean isValid(String phoneNumber) {
-        return phoneNumber != null
-                && (isInternationalNumber(phoneNumber) || isLocalNumber(phoneNumber));
+        return phoneNumber != null && isInternationalNumber(phoneNumber);
     }
 
     private boolean isInternationalNumber(String phoneNumber) {
         return phoneNumber.length() == internationalLength
                 && phoneNumber.startsWith(internationalPrefix)
-                && phoneNumber.substring(internationalPrefix.length()).chars().allMatch(Character::isDigit);
+                && phoneNumber.substring(1).chars().allMatch(Character::isDigit);
     }
 
-    private boolean isLocalNumber(String phoneNumber) {
-        return phoneNumber.length() == nationalLength
-                && phoneNumber.startsWith(nationalPrefix)
-                && phoneNumber.substring(nationalPrefix.length()).chars().allMatch(Character::isDigit);
+    public String getInternationalPrefix() {
+        return internationalPrefix;
     }
 }
