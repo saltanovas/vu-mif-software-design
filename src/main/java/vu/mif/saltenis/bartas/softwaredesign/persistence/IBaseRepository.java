c29d5c9ca5d4bfc2b7854523df8534bf9833bcff
saltanovas <zumbass@gmail.com>
2021-11-09 21:23:43 +0200

    Base repository

---
+++ src/main/java/vu/mif/saltenis/bartas/softwaredesign/persistence/IBaseRepository.java
@@ -0,0 +1,10 @@
+package vu.mif.saltenis.bartas.softwaredesign.persistence;
+
+import vu.mif.saltenis.bartas.softwaredesign.share.BaseEntity;
+import java.io.IOException;
+
+public interface IBaseRepository<T extends BaseEntity<T>> {
+    T getById(int id);
+    void removeAndFlush(T entity) throws IOException;
+    void saveAndFlush(T entity) throws IOException;
+}
